ab34357e00e3ff88a44026655610b111834cc96a
Jorge G <jorgeald77@gmail.com>
2022-06-16 15:52:18 -0500

    Se creo el template profile.html, se creo la ruta necesaria, y la vista básica

--- app/profiles/views.py
+++ app/profiles/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+
+class ViewProfile(LoginRequiredMixin, View):
+    login_url = '/auth/login/'
+    redirect_field_name = 'redirect_to'
+
+    def get(self, request):
+        # TODO Funcionalidad que muestra la info del usuario basada en su perfil
+        return render(request, 'profile.html')
+
+    def post(self, request):
+        # TODO Funcionalidad para guardar o actualizar el registro.
+        pass
