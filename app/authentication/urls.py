5a074356825632e02e99f1ea7654289023bc15e5
Jorge G <jorgeald77@gmail.com>
2022-06-15 23:45:31 -0500

    Se creo la funcionalidad para iniciar sesion

--- app/authentication/urls.py
+++ app/authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from authentication.views import ViewRegister, salir
+from authentication.views import ViewRegister, salir, entrar
 
 urlpatterns = [
     path('registro/', ViewRegister.as_view(), name='register'),
-    path('salir/', salir, name='logout'),
+    path('logout/', salir, name='logout'),
+    path('login/', entrar, name='login'),
 ]
--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,8 +1,8 @@
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 class ViewRegister(View):
@@ -22,6 +22,22 @@ class ViewRegister(View):
             for msg in form.error_messages:
                 messages.error(request, form.error_messages[msg])
             return render(request, 'register.html', {'form_register': form})
+
+
+def entrar(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Bienvenido al Blog {username}')
+                return redirect('home')
+
+    form = AuthenticationForm
+    return render(request, 'login.html', {'form_login': form})
 
 
 def salir(request):
