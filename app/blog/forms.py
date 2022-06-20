aa2ecbbcf2f00b26e0275ddbfaeda7f7c7d6bd30
Jorge G <jorgeald77@gmail.com>
2022-06-20 09:43:22 -0500

    Se creo el modelo y Formulario para realizar la suscripcion a los Posts

---
+++ app/blog/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from blog.models import Suscriber
+
+
+class FormSuscriber(forms.ModelForm):
+    class Meta:
+        model = Suscriber
+        fields = ('nombre', 'email')
+        widgets = {
+            'nombre': forms.TextInput(attrs={'required': True}),
+            'email': forms.EmailField(attrs={'required': True})
+        }
--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Suscriber(models.Model):
+    nombre = models.CharField(max_length=64, verbose_name='Nombre')
+    email = models.EmailField(max_length=64, verbose_name='Correo electrónico')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Suscriptor: {self.nombre}"
+
+    class Meta:
+        db_table = 'suscribers'
+        verbose_name = 'Suscriber',
+        verbose_name_plural = 'Suscribers'
+        ordering = ['nombre']
