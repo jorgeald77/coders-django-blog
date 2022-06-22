8bd4d83911111f717b37ca9f026feb2c403e43e2
Jorge G <jorgeald77@gmail.com>
2022-06-21 20:10:23 -0500

    Se dio diseño a dashboard del usuario

--- app/users/views.py
+++ app/users/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    # TODO Obtener los posts creados por el usuario autenticado  desde la DB
+    posts = [
+        {'id': 1, 'title': 'lorem10', 'published': '2022-05-10'},
+        {'id': 1, 'title': 'lorem10', 'published': '2022-05-10'},
+    ]
+
+    return render(request, 'dashboard.html', {
+        'posts': posts,
+        'author': request.user.profile.nombreCompleto()
+    })
