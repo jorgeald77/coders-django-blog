e77ab59a2885c72feb18dec53d4b8172371668df
Jorge G <jorgeald77@gmail.com>
2022-06-25 17:39:58 -0500

    Se renombraron los templates de posts

--- app/posts/views.py
+++ app/posts/views.py
@@ -8,52 +8,46 @@ from django.contrib import messages
 from posts.forms import PostForm
 from posts.models import Post
 
+
 def viewPosts(request):
     posts = Post.objects.all()
+    return render(request, "post/list.html", {'posts': posts})
 
-    return render(request, "post/viewposts.html", {'posts': posts})
 
-
+# TODO Hacer uso de MessageMixin
 class ViewCreate(LoginRequiredMixin, View):
-    login_url =  '/auth/login'      # Si el usuario no está authenticado, será redirigido a esta ruta
+    login_url = '/auth/login'
     redirect_field_name = 'redirect_to'
 
-    # Si la petición es GET entra en esta función
     def get(self, request):
         form = PostForm()
-        return render(request, 'post/postform.html', {"form": form})
+        return render(request, 'post/create.html', {"form": form})
 
     def post(self, request):
-
         form = PostForm(request.POST, request.FILES)
-        
         if form.is_valid():
-
             blogPost = form.save(commit=False)
-            
-            blogPost.user =  request.user
-            
+            blogPost.user = request.user
             blogPost.save()
-
-
             messages.success(request, f'El post "{blogPost.title}", se ha guardado.')
-            
             return redirect('viewPosts')
 
 
+# TODO Hacer uso de UserPassesTestMixin y MessagesMixin
 class ViewUpdate(LoginRequiredMixin, UpdateView):
-    login_url =  '/auth/login'
+    login_url = '/auth/login'
     redirect_field_name = 'redirect_to'
 
     model = Post
     form_class = PostForm
-    template_name = 'post/updatepostform.html'
+    template_name = 'post/edit.html'
 
 
+# TODO Hacer uso de UserPassesTestMixin y  MessagesMixin
 class ViewDelete(LoginRequiredMixin, DeleteView):
-     login_url =  '/auth/login'
-     redirect_field_name = 'redirect_to'
+    login_url = '/auth/login'
+    redirect_field_name = 'redirect_to'
 
-     model = Post
-     template_name = 'post/viewposts.html'
-     success_url = reverse_lazy('viewPosts')
+    model = Post
+    template_name = 'post/delete.html'
+    success_url = reverse_lazy('viewPosts')
